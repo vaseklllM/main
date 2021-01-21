d14f312cd947ae9baaa7b7534cf395a7e8ec2661
vasek <vasia.motnin@gmail.com>
2021-01-21 23:32:52 +0200

    Add layout

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -1,12 +1,162 @@
-import { ReactElement } from "react"
-import { DatePicker } from "antd"
+import React from "react"
+import { Form, Input, Tooltip, Checkbox, Button, Row, Layout } from "antd"
+import { QuestionCircleOutlined } from "@ant-design/icons"
+import classes from "./style.module.scss"
 
-interface Props {}
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+}
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+}
 
-export default function Registration({}: Props): ReactElement {
+const { Header, Footer, Sider, Content } = Layout
+
+export default function RegistrationForm() {
+  const [form] = Form.useForm()
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values)
+  }
+
   return (
-    <div>
-      <DatePicker />
-    </div>
+    <Layout>
+      <Row justify='center' align='middle'>
+        <Form
+          {...formItemLayout}
+          form={form}
+          name='register'
+          onFinish={onFinish}
+          initialValues={{
+            residence: ["zhejiang", "hangzhou", "xihu"],
+            prefix: "86",
+          }}
+          scrollToFirstError
+        >
+          <Form.Item
+            name='email'
+            label='E-mail'
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name='password'
+            label='Password'
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name='confirm'
+            label='Confirm Password'
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve()
+                  }
+
+                  return Promise.reject(
+                    "The two passwords that you entered do not match!"
+                  )
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name='nickname'
+            label={
+              <span>
+                Nickname&nbsp;
+                <Tooltip title='What do you want others to call you?'>
+                  <QuestionCircleOutlined />
+                </Tooltip>
+              </span>
+            }
+            rules={[
+              {
+                required: true,
+                message: "Please input your nickname!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name='agreement'
+            valuePropName='checked'
+            rules={[
+              {
+                validator: (_, value) =>
+                  value ? Promise.resolve() : Promise.reject("Should accept agreement"),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              I have read the <a href=''>agreement</a>
+            </Checkbox>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type='primary' htmlType='submit'>
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </Row>
+    </Layout>
   )
 }
