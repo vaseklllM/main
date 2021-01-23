2417140124cd2581141a4e6569312c82dda4648a
vasek <vasia.motnin@gmail.com>
2021-01-23 16:16:20 +0200

    add login

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const { SERVER_PORT, DB_URL, FRONTEND_URL } = require("../utils/config")
-const app = express()
 const mongoose = require("mongoose")
 const createDefData = require("./createDefaultData")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const passport = require("passport")
+
+const app = express()
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -30,7 +31,6 @@ async function start() {
     })
 
     createDefData()
-    
   } catch (error) {
     console.log(error)
   }
--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -1,42 +1,49 @@
 const { Router } = require("express")
 const router = Router()
 
-// const bcrypt = require("bcryptjs")
-// const jwt = require("jsonwebtoken")
-// const { PASSPORT_SECRET_KEY } = require("../../../utils/config")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { PASSPORT_SECRET_KEY } = require("../../../utils/config")
 
-// const User = require("../../models/User")
+const User = require("../../models/User")
 
-router.get("/login", async (req, res) => {
-  
-  console.log('--------- NEW request ----------')
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body
 
+  /** валидация */
+  if (!email || (typeof email === "string" && email === "")) {
+    res.status(401).json({ message: "Введите email" })
+  }
+  if (typeof email !== "string") {
+    res.status(401).json({ message: "Неверный email" })
+  }
+  if (!password || (typeof password === "string" && password === "")) {
+    res.status(401).json({ message: "Введите пароль" })
+  }
+  if (typeof password !== "string") {
+    res.status(401).json({ message: "Пароль должен быть строкой" })
+  }
 
-  res.send('Hello World')
+  /** Вход в аккаунт */
+  const candidate = await User.findOne({ email })
 
-
-  // const { email, password } = req.body
-
-
-  // const candidate = await User.findOne({ email })
-
-  // if (candidate) {
-  //   const isMatch = await bcrypt.compare(password, candidate.password)
-  //   if (isMatch) {
-  //     const token = getToken(candidate)
-  //     res.status(201).json({ message: "Успешная авторизация", token })
-  //   } else {
-  //     res.status(401).json({ message: "Неверный логин или пароль" })
-  //   }
-  // } else {
-  //   res.status(401).json({ message: "Пользователь не зарегистрирован" })
-  // }
+  if (candidate) {
+    const isMatch = await bcrypt.compare(password, candidate.password)
+    if (isMatch) {
+      const token = getToken(candidate)
+      res.status(201).json({ message: "Успешная авторизация", token })
+    } else {
+      res.status(401).json({ message: "Неверный логин или пароль" })
+    }
+  } else {
+    res.status(401).json({ message: "Пользователь не зарегистрирован" })
+  }
 })
 
-// function getToken(candidate) {
-//   const payload = { id: candidate._id, email: candidate.email }
-//   const options = { expiresIn: 86400 }
-//   return "Bearer " + jwt.sign(payload, PASSPORT_SECRET_KEY, options)
-// }
+function getToken(candidate) {
+  const payload = { id: candidate._id, email: candidate.email }
+  const options = { expiresIn: 86400 }
+  return "Bearer " + jwt.sign(payload, PASSPORT_SECRET_KEY, options)
+}
 
 module.exports = router
