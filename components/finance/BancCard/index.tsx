4ced8f16c70db821e28ab5294a8899dfeffe8552
vasek <vasia.motnin@gmail.com>
2021-01-30 19:03:19 +0200

    Update add bank card

--- components/finance/BancCard/index.tsx
+++ components/finance/BancCard/index.tsx
@@ -1,7 +1,8 @@
-import React, { ReactElement } from "react"
-import { Card, Col } from "antd"
+import React, { ReactElement, useState } from "react"
+import { Col, Button, Tooltip, Typography } from "antd"
 import classes from "./style.module.scss"
 import { txt } from "../../../utils"
+import { PlusOutlined, MinusOutlined } from "@ant-design/icons"
 
 interface Props {
   title?: string
@@ -11,12 +12,28 @@ interface Props {
 
 export default function BancCard(props: Props): ReactElement {
   const { title = "card title", className, children } = props
+  const { Title } = Typography
+  const [isAdd, setIsAdd] = useState(false)
 
   return (
     <Col className={txt.join([classes.card, className])}>
-      <Card title={title} bordered={false}>
-        {children}
-      </Card>
+      <div>
+        <div className={classes.title}>
+          <Title level={5} className={classes.title_text}>
+            {title}
+          </Title>
+          <Tooltip title={isAdd ? "Скрити" : "Підключити"}>
+            <Button
+              onClick={() => setIsAdd(!isAdd)}
+              type='primary'
+              shape='circle'
+              icon={isAdd ? <MinusOutlined /> : <PlusOutlined />}
+            />
+          </Tooltip>
+        </div>
+        {isAdd && <div className={classes.hr} />}
+        {isAdd && <div className={classes.body}>{children}</div>}
+      </div>
     </Col>
   )
 }
--- components/finance/ConnectedBankCardBlock/index.tsx
+++ components/finance/ConnectedBankCardBlock/index.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react"
 import FinanceContentBlock from "../FinanceContentBlock"
 import classes from "./style.module.scss"
 import BancCard from "../BancCard"
-import { Row, Typography, Space } from "antd"
+import { Row, Typography, Space, Button } from "antd"
 import AddMonoBank from "./AddMonoBank"
 
 interface Props {}
@@ -14,13 +14,16 @@ export default function ConnectedBankCardBlock({}: Props): ReactElement {
     <FinanceContentBlock>
       <Title level={5}>Банківські карти</Title>
       <Text type='secondary'>
-        Підключіть банківські карти для регулярного автоматичного перегляду змін на рахунках.
+        Підключіть банківські карти для регулярного автоматичного перегляду змін на
+        рахунках.
       </Text>
       <Row className={classes.bank_cards}>
-        <Space size={15} wrap>
+        <AddMonoBank className={classes.bank_card} />
+        {/* <Button type="primary" >Добавити карту</Button> */}
+        {/* <Space size={15} wrap>
           <AddMonoBank className={classes.bank_card} />
           <BancCard className={classes.bank_card} title='Приватбанк'></BancCard>
-        </Space>
+        </Space> */}
       </Row>
     </FinanceContentBlock>
   )
