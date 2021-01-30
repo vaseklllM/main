43f57c61099615ba47e3cfbeb8e4f5cd7a41312e
vasek <vasia.motnin@gmail.com>
2021-01-30 23:32:17 +0200

    Update monobank token input

--- components/finance/ConnectedBankCardBlock/AddMonoBank/index.tsx
+++ components/finance/ConnectedBankCardBlock/AddMonoBank/index.tsx
@@ -1,9 +1,11 @@
-import React, { ReactElement } from "react"
+import React, { ChangeEvent, ReactElement, useState } from "react"
 import BancCard from "../../BancCard"
 import { Button, Input, Row, Typography } from "antd"
 import { txt } from "../../../../utils"
 import classes from "./style.module.scss"
 import Link from "next/link"
+import { status } from "../../../../utils/status"
+import InputActiveIcon from "./InputActiveIcon"
 
 interface Props {
   className?: string
@@ -12,9 +14,25 @@ interface Props {
 export default function AddMonoBank(props: Props): ReactElement {
   const { className } = props
   const { Text } = Typography
+  const [token, setToken] = useState("")
+  const [isActiveToken, setIsActiveToken] = useState(status.no_data)
 
   const goMonoBtnText = "Получити токен"
   const inputPlaceholder = "Токен"
+
+  async function changeToken(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target
+    setToken(value)
+    if (value.length === 44) {
+      setIsActiveToken(status.loading)
+
+      setTimeout(() => {
+        setIsActiveToken(status.successful)
+      }, 1000)
+    } else if (isActiveToken !== status.no_data) {
+      setIsActiveToken(status.no_data)
+    }
+  }
 
   return (
     <BancCard className={txt.join([className])} title='Монобанк'>
@@ -33,8 +51,17 @@ export default function AddMonoBank(props: Props): ReactElement {
           </Button>
         </Link>
       </Row>
-      <Row>
-        <Input className={classes.token_input} placeholder={inputPlaceholder} />
+      <Row className={classes.token_input_row}>
+        <Input
+          className={classes.token_input}
+          placeholder={inputPlaceholder}
+          onChange={changeToken}
+          value={token}
+        />
+        <InputActiveIcon
+          className={classes.token_status_icon}
+          isActiveToken={token !== "" && isActiveToken}
+        />
       </Row>
     </BancCard>
   )
