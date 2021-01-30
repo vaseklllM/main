7fe5037bdfd8be422f3ed26be799ea49338822c5
vasek <vasia.motnin@gmail.com>
2021-01-30 21:43:03 +0200

    Update bank card

--- components/finance/BancCard/index.tsx
+++ components/finance/BancCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react"
+import React, { ReactElement, useEffect, useRef, useState } from "react"
 import { Col, Button, Tooltip, Typography } from "antd"
 import classes from "./style.module.scss"
 import { txt } from "../../../utils"
@@ -10,30 +10,77 @@ interface Props {
   children?: ReactElement | ReactElement[]
 }
 
+const ANIMATION_TIME = 700
+
 export default function BancCard(props: Props): ReactElement {
   const { title = "card title", className, children } = props
   const { Title } = Typography
   const [isAdd, setIsAdd] = useState(false)
+  const [isShowBody, setIsShowBody] = useState(false)
+  const [bodyHeight, setBodyHeight] = useState(0)
+  const [bodyWidth, setBodyWidth] = useState(0)
+
+  const bodyRef = useRef(null)
+  const mainRef = useRef(null)
+
+  useEffect(() => {
+    if (bodyRef.current && isAdd) {
+      setBodyWidth(mainRef.current.offsetWidth)
+      setTimeout(() => {
+        setBodyHeight(bodyRef.current.offsetHeight)
+      }, ANIMATION_TIME / 2)
+    } else {
+      setBodyWidth(0)
+      setBodyHeight(0)
+    }
+  }, [isShowBody, isAdd])
+
+  function changeIsAdd() {
+    if (isAdd) {
+      setIsAdd(false)
+      setTimeout(() => {
+        setIsShowBody(false)
+      }, ANIMATION_TIME)
+    } else {
+      setIsShowBody(true)
+      setIsAdd(true)
+    }
+  }
+
+  const t = ANIMATION_TIME / 1000
 
   return (
-    <Col className={txt.join([classes.card, className])}>
-      <div>
-        <div className={classes.title}>
-          <Title level={5} className={classes.title_text}>
-            {title}
-          </Title>
-          <Tooltip title={isAdd ? "Скрити" : "Підключити"}>
-            <Button
-              onClick={() => setIsAdd(!isAdd)}
-              type='primary'
-              shape='circle'
-              icon={isAdd ? <MinusOutlined /> : <PlusOutlined />}
-            />
-          </Tooltip>
+    <div className={classes.main} ref={mainRef}>
+      <Col
+        className={txt.join([classes.card, isAdd && classes.active_card, className])}
+        style={{
+          transition: `width ${t}s, height ${t}s ease-in-out`,
+          height: `${bodyHeight + 84}px`,
+          width: `${bodyWidth + 280}px`,
+        }}
+      >
+        <div>
+          <div className={classes.title}>
+            <Title level={5} className={classes.title_text}>
+              {title}
+            </Title>
+            <Tooltip title={isAdd ? "Приховати" : "Підключити"}>
+              <Button
+                onClick={changeIsAdd}
+                type='primary'
+                shape='circle'
+                icon={isAdd ? <MinusOutlined /> : <PlusOutlined />}
+              />
+            </Tooltip>
+          </div>
+          {isShowBody && <div className={classes.hr} />}
+          {isShowBody && (
+            <div className={classes.body} ref={bodyRef}>
+              {children}
+            </div>
+          )}
         </div>
-        {isAdd && <div className={classes.hr} />}
-        {isAdd && <div className={classes.body}>{children}</div>}
-      </div>
-    </Col>
+      </Col>
+    </div>
   )
 }
--- components/finance/ConnectedBankCardBlock/AddMonoBank/index.tsx
+++ components/finance/ConnectedBankCardBlock/AddMonoBank/index.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from "react"
 import BancCard from "../../BancCard"
-import { Button } from "antd"
+import { Button, Row, Typography } from "antd"
+import { txt } from "../../../../utils"
+import classes from "./style.module.scss"
 
 interface Props {
   className?: string
@@ -8,9 +10,17 @@ interface Props {
 
 export default function AddMonoBank(props: Props): ReactElement {
   const { className } = props
+  const { Text } = Typography
 
   return (
-    <BancCard className={className} title='Монобанк'>
+    <BancCard className={txt.join([className])} title='Монобанк'>
+      <Row>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit nam ipsum
+          laudantium pariatur repudiandae assumenda, dicta incidunt quis. Quo molestias
+          quia iure ex fugit unde modi inventore ipsam eius mollitia.
+        </Text>
+      </Row>
       <Button type='primary'>Підключити</Button>
     </BancCard>
   )
