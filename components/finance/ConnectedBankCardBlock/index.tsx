45c009b4bb332804585792e4f0f643bc162a604f
vasek <vasia.motnin@gmail.com>
2021-02-07 19:17:44 +0200

    small update

--- components/finance/ConnectedBankCardBlock/index.tsx
+++ components/finance/ConnectedBankCardBlock/index.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement } from "react"
 import FinanceContentBlock from "../FinanceContentBlock"
 import classes from "./style.module.scss"
-import BancCard from "../BancCard"
-import { Row, Typography, Space, Button } from "antd"
+import { Row, Typography } from "antd"
 import AddMonoBank from "./AddMonoBank"
 
 interface Props {}
@@ -12,18 +11,13 @@ export default function ConnectedBankCardBlock({}: Props): ReactElement {
 
   return (
     <FinanceContentBlock>
-      <Title level={5}>Банківські карти</Title>
+      <Title level={5}>Підключення банківських карт</Title>
       <Text type='secondary'>
         Підключіть банківські карти для регулярного автоматичного перегляду змін на
         рахунках.
       </Text>
       <Row className={classes.bank_cards}>
         <AddMonoBank className={classes.bank_card} />
-        {/* <Button type="primary" >Добавити карту</Button> */}
-        {/* <Space size={15} wrap>
-          <AddMonoBank className={classes.bank_card} />
-          <BancCard className={classes.bank_card} title='Приватбанк'></BancCard>
-        </Space> */}
       </Row>
     </FinanceContentBlock>
   )
