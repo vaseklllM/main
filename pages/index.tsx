0c72f36e974bd9d3b6bfc831bb4bb31e34deeba7
vasek <vasia.motnin@gmail.com>
2021-05-10 15:36:50 +0300

    reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import App from "@/components/App"
-import { status } from "@/enums"
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <Head>
         <title>Все приложения в одном месте</title>
       </Head>
-      {status[status.no_data]}
       <App />
     </div>
   )
